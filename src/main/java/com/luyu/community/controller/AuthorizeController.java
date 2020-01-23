cb264f1d7a5e89fa0c717f59fac7b890f430072c
luyu <1028695233@qq.com>
2020-01-23 12:38:52 +0800

    modify perpose

--- src/main/java/com/luyu/community/controller/AuthorizeController.java
+++ src/main/java/com/luyu/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.luyu.community.dto.AccessTokenDTO;
 import com.luyu.community.dto.GithubUser;
 import com.luyu.community.provide.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,23 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
     @GetMapping("/rallback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state){
         AccessTokenDTO   accessTokenDTO    = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("427f855d1394563cb658");
+        accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/rallback");
-        accessTokenDTO.setClient_secret("c2e87d0e2bf0a71bfcfcadf3c460555ea9945644");
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setState(state);
         String token = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(token);
